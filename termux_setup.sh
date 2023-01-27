7b07a79a5e44374991d377335c224c99fb0ac7b0
Leonardo Sousa <45713275+Visto-Preto@users.noreply.github.com>
2023-01-27 15:17:47 -0300

    Update termux_setup.sh

--- termux_setup.sh
+++ termux_setup.sh
@@ -3,14 +3,14 @@
 echo -e "\n\033[1;36mPROOT-DISTRO UBUNTU XFCE4 Installer \033[0m\n\n"
 echo -e "\n\033[1;36mSCRIPIT 1/2\033[0m\n\n"
 sleep 1
-echo -e "\033[1,36mWait...\033[0m\n"
+echo -e "\033[1;36mWait...\033[0m\n"
 sleep 3
-echo -e "\033[1,36mInstalling\033[0m\n\n"
+echo -e "\033[1;36mInstalling\033[0m\n\n"
 apt install x11-repo
 apt update
-apt install tigervnc xorg-xhost pulseaudio
-apt install proot-distro
+apt install -y tigervnc xorg-xhost pulseaudio proot-distro
 proot-distro install ubuntu
+
 cat > /usr/bin/ubuntu <<- _EOF_
 #!/usr/bin/env bash
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
@@ -26,4 +26,4 @@ _EOF_
 
 chmod +x /usr/bin/ubuntu startvnc
 
-echo -e "\n\n\033[1,36mFinish!\033[0m\n\n"
+echo -e "\n\n\033[1;36mFinish!\033[0m\n\n"
