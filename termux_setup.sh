ef7f47be7720d372f965d4efe7ace4763039cbf8
Leonardo Sousa <45713275+Visto-Preto@users.noreply.github.com>
2023-01-27 16:51:39 -0300

    Update termux_setup.sh

--- termux_setup.sh
+++ termux_setup.sh
@@ -13,14 +13,16 @@ proot-distro install ubuntu
 
 echo '''#!/usr/bin/env bash
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
-proot-distro login ubuntu''' >> $PREFIX/usr/bin/ubuntu
+proot-distro login ubuntu
+''' >> ubuntu
 
 echo '''#!/usr/bin/env bash
 vncserver -geometry 1280x720 -noxstartup -listen tcp :1
 DISPLAY=:1 xhost +
-''' >> $PREFIX/usr/bin/startvnc
+''' >> startvnc
 
-chmod +x $PREFIX/usr/bin/ubuntu 
-chmod +x $PREFIX/usr/bin/startvnc
+chmod +x ubuntu startvnc
+mv startvnc ubuntu
+rm -rf startvnc ubuntu
 
 echo -e "\n\n\033[1;36mFinish!\033[0m\n\n"
