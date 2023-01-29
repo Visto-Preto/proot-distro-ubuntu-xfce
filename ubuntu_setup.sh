d14897fcd7ef61573257cf8b792ac39b17b403fb
Visto Preto <45713275+Visto-Preto@users.noreply.github.com>
2023-01-29 20:36:40 -0300

    Update ubuntu_setup.sh

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -7,15 +7,12 @@ echo -e "\033[1;36mWait...\033[0m\n"
 sleep 3
 echo -e "\033[1;36mInstalling XFCE4\033[0m\n\n"
 apt update
-apt install -y xfwm4 xfce4-panel xfce4-settings xfce4-session xfce4-terminal xfdesktop4 xfce4-taskmanager dbus-x11 elementary-xfce-icon-theme --no-install-recommends
-apt install -y thunar geany mousepad tumbler tumbler-plugins-extra ffmpegthumbnailer pulseaudio xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin
-apt-get install -y audacious parole ristretto
+apt install -y xfce4 xfce4-goodies mugshot dbus-x11 thunar geany tumbler tumbler-plugins-extra ffmpegthumbnailer pulseaudio xfce4-pulseaudio-plugin xfce4-whiskermenu-plugin
 
 echo '''#!/bin/env bash
 DISPLAY=:1 startxfce4
-''' >> startdesktop
+''' >> /bin/startdesktop
 
-chmod +x startdesktop
-mv startdesktop /bin/
+chmod +x /bin/startdesktop
 
 echo -e "\n\n\033[1;36mFinish!\033[0m\n\n"
